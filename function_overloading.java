42a8f8112a85f8d3d11cd32e01409f0c6839338a
OnlyPooja <poojajoshi84478@gmail.com>
2023-04-06 01:10:56 +0530

    modify

---
+++ function_overloading.java
@@ -0,0 +1,8 @@
+ import java.util.Scanner;
+ class function_overloading {
+    public static void main(String args[])
+    {
+         Scanner sc=new Scanner(System.in);
+         filhaal ke ;liye ku
+    }
+}
